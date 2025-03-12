606b1492f0469b529ef316ef381c46b1fa77b7b0
Nicolás Opazo <nicolas1212opazo@gmail.com>
2025-03-12 16:10:06 -0300

    Carpeta Actualizada REM 2025
    Actualización de carpeta, nombres y archivos para desarrollo.

---
+++ A01.sql
@@ -0,0 +1,20 @@
+'''
+CREATE TABLE "A01" (
+    "REM" VARCHAR(255),
+    "Seccion" VARCHAR(255),
+    "IdEstablecimiento" INT,
+    "PK" VARCHAR(255),
+    "IdServicio" INT,
+    "Ano" INT,
+    "Mes" INT,
+    "IdRegion" INT,
+    "IdComuna" INT,
+    "CodigoPrestacion" VARCHAR(50),
+    "Tipo Control" VARCHAR(50),
+    "Profesional" VARCHAR(50),
+    "Tipo de Caracteristica" VARCHAR(255),
+    "Caracteristica" VARCHAR(255),
+    "Variable" VARCHAR(50),
+    "Cantidad" INT
+);
+'''
